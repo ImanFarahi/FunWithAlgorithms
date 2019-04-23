1a238d491cdfd5040e2c679ba5284d9ff37c72af
Iman Farahi <43908633+ImanFarahi@users.noreply.github.com>
2019-04-23 20:18:15 +0430

    Update strongPasswordChecker_2019-04-23.py

--- strongPasswordChecker_2019-04-23.py
+++ strongPasswordChecker_2019-04-23.py
@@ -8,12 +8,12 @@
         return (lower + upper + digit)
     
     def strongPasswordChecker(self, s):
-
         str_len= len(s)
+        
         missing_type = self.get_missing_type(s)
         if str_len < 6: return max(missing_type, 6 - str_len)
         
-        change = one= two= 0
+        change = one = two= 0
         p = 2 
         while p < str_len:
             if s[p] == s[p-1] == s[p-2]: 
@@ -27,12 +27,10 @@
                 elif length % 3 == 1: two += 2
             else:
                 p += 1
-                
         if str_len <= 20: return max(missing_type, change)
         
         delete = str_len - 20
         change -= min(delete, one)
         change -= min(max(delete - one, 0), two ) / 2
         change -= max(delete - one - two, 0) / 3
-        return delete + max(missing_type, change)     
-
+        return delete + max(missing_type, change)
