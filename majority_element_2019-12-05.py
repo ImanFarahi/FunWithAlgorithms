33f4b0f30e57138883d1a53895c567f646a1a8c6
Iman Farahi <43908633+ImanFarahi@users.noreply.github.com>
2019-05-14 08:48:25 +0430

    Update majority_element_2019-12-05.py

--- majority_element_2019-12-05.py
+++ majority_element_2019-12-05.py
@@ -60,7 +60,7 @@ class Solution5(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        m = len(nums) / 3; v, t, c = [], [sys.maxsize, sys.maxsize], [0, 0]
+        m = len(nums) / 3; v, t, c = [], [sys.maxsize]*2, [0, 0]
         for num in nums:
             if t[0] == num:
                 c[0] += 1 
